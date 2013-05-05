6f694c75d20924b6bbbf8e93c278fd00ab5c201e
Matheo Fiebiger <matheof@Matheos-Mac.local>
2013-05-04 20:12:42 -0500

    app inicial

--- frontend/Transporte Activo/Resources/app.js
+++ frontend/Transporte Activo/Resources/app.js
@@ -1,64 +1,113 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
-
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
-
-
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
+var menuWindow = Ti.UI.createWindow({
+	top : 0,
+	left : 0,
+	width : 150
 });
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
+menuWindow.open();
+//// ---- Menu Table
+// Menu Titles
+var menuTitles = [{
+	title : 'Menu 1'
+}, {
+	title : 'Menu 2'
+}, {
+	title : 'Menu 3'
+}, {
+	title : 'Menu 4'
+}, {
+	title : 'Menu 5'
+}, {
+	title : 'Menu 6'
+}];
+// Tableview
+var tableView = Ti.UI.createTableView({
+	data : menuTitles
 });
+menuWindow.add(tableView);
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+//// ---- Window with navigationGroup
+var navWindow = Ti.UI.createWindow({
+	width : 320 // Set the width of the sliding window to avoid cut out from animation
 });
-
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
+navWindow.open();
+// Main window
+var win = Ti.UI.createWindow({
+	title : 'Main Window',
+	backgroundColor : '#28292c',
+	barColor : '#28292c',
+	moving : false, // Custom property for movement
+	axis : 0 // Custom property for X axis
 });
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
+// NavigationGroup
+var navGroup = Ti.UI.iPhone.createNavigationGroup({
+	window : win
 });
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+navWindow.add(navGroup);
+// Top left button
+var menuButton = Ti.UI.createButton({
+	title : 'Menu',
+	toggle : false // Custom property for menu toggle
 });
+win.setLeftNavButton(menuButton);
 
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+menuButton.addEventListener('click', function(e) {
+	// If the menu is opened
+	if (e.source.toggle == true) {
+		navWindow.animate({
+			left : 0,
+			duration : 400,
+			curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+		});
+		e.source.toggle = false;
+	}
+	// If the menu isn't opened
+	else {
+		navWindow.animate({
+			left : 150,
+			duration : 400,
+			curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+		});
+		e.source.toggle = true;
+	}
+});
+win.addEventListener('touchstart', function(e) {
+	// Get starting horizontal position
+	e.source.axis = parseInt(e.x);
+});
+win.addEventListener('touchmove', function(e) {
+	// Subtracting current position to starting horizontal position
+	var coordinates = parseInt(e.globalPoint.x) - e.source.axis;
+	// Detecting movement after a 20px shift
+	if (coordinates > 20 || coordinates < -20) {
+		e.source.moving = true;
+	}
+	// Locks the window so it doesn't move further than allowed
+	if (e.source.moving == true && coordinates <= 150 && coordinates >= 0) {
+		// This will smooth the animation and make it less jumpy
+		navWindow.animate({
+			left : coordinates,
+			duration : 20
+		});
+		// Defining coordinates as the final left position
+		navWindow.left = coordinates;
+	}
+});
+win.addEventListener('touchend', function(e) {
+	// No longer moving the window
+	e.source.moving = false;
+	if (navWindow.left >= 75 && navWindow.left < 150) {
+		// Repositioning the window to the right
+		navWindow.animate({
+			left : 150,
+			duration : 300
+		});
+		menuButton.toggle = true;
+	} else {
+		// Repositioning the window to the left
+		navWindow.animate({
+			left : 0,
+			duration : 300
+		});
+		menuButton.toggle = false;
+	}
+}); 
