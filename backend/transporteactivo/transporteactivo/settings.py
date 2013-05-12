6ad45b358ce4393ed75af6a77b5da08ab4617a32
Guillermo Álvarez <guialante@gmail.com>
2013-05-11 23:55:33 -0500

    ...

--- backend/transporteactivo/transporteactivo/settings.py
+++ backend/transporteactivo/transporteactivo/settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     'sgco',
     'mio',
     'rest_framework',
+    'corsheaders',
     'django_extensions',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
