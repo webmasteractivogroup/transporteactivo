73e0bc21e61e86ab1fc8832bf906ae43bbd5fffe
Matheo <matheof@hotmail.com>
2013-05-15 20:18:41 -0500

    Paradas cercanas Ok, Con request on map touch.

--- frontend/Transporte Activo/Resources/ui/common/PlanearViaje.js
+++ frontend/Transporte Activo/Resources/ui/common/PlanearViaje.js
@@ -8,26 +8,24 @@ Titanium.Geolocation.distanceFilter = 10;
 
 var mapview;
 var paradas = new Array(); 
+var updateMapTimeout;
+var viewContenedora;
 
 function PlanearViaje() {
 Ti.API.log('CREANDO MAPA ');
-	var mountainView = Titanium.Map.createAnnotation({
-		latitude : 37.390749,
-		longitude : -122.081651,
-		title : "Appcelerator Headquarters",
-		subtitle : 'Mountain View, CA',
-		pincolor : Titanium.Map.ANNOTATION_RED,
-		animate : true,
-		leftButton : '../images/appcelerator_small.png',
-		myid : 1 // Custom property to uniquely identify this annotation.
+	
+	viewContenedora = Ti.UI.createView({
+		Height: Ti.FILL,
+		Width: Ti.FILL
 	});
 
 	mapview = Titanium.Map.createView({
+		Height: Ti.FILL,
+		Width: Ti.FILL,
 		mapType : Titanium.Map.STANDARD_TYPE,
 		animate : true,
 		regionFit : true,
-		userLocation : true,
-		annotations : [mountainView]
+		userLocation : true
 	});
 
 	// Handle click events on any annotations on this map.
@@ -41,19 +39,50 @@ Ti.API.log('CREANDO MAPA ');
 			Ti.API.info("Annotation " + evt.title + ", left button clicked.");
 		}
 	});
+	
+	var goToMe = Titanium.UI.createButton({
+    backgroundImage:'/images/userloc.png',
+    top:10,
+    right:10,
+    width: 40,
+    height: 40
 
-	return mapview;
+});
+
+	goToMe.addEventListener('click',function(e){
+		Titanium.Geolocation.getCurrentPosition(function(e) {
+		var region = {
+			latitude : e.coords.latitude,
+			longitude : e.coords.longitude,
+			animate : true,
+			latitudeDelta : 0.01,
+			longitudeDelta : 0.01
+		};
+
+		mapview.setLocation(region);
+		});
+	});
+	
+	viewContenedora.add(mapview);
+	viewContenedora.add(goToMe);
+
+	return viewContenedora;
 };
 
+var times =0;
+
 function getLocation() {
+	if(times===0)
+	{
+	times++;
 	//Get the current position and set it to the mapview
 	Titanium.Geolocation.getCurrentPosition(function(e) {
 		var region = {
 			latitude : e.coords.latitude,
 			longitude : e.coords.longitude,
 			animate : true,
-			latitudeDelta : 0.03,
-			longitudeDelta : 0.03
+			latitudeDelta : 0.01,
+			longitudeDelta : 0.01
 		};
 
 		mapview.setLocation(region);
@@ -64,8 +93,27 @@ function getLocation() {
 			getLocation();
 		});
 		
+		mapview.addEventListener('regionchanged', function(e) {
+		
+		 Ti.API.log('REGION CHANGED');
+	var region = {
+			latitude : e.latitude,
+			longitude : e.longitude,
+			animate : true,
+			latitudeDelta : e.latitudeDelta,
+			longitudeDelta : e.longitudeDelta
+		};
+		if (updateMapTimeout) clearTimeout(updateMapTimeout);
+ 
+    updateMapTimeout = setTimeout(function() {
+        // update your map
+    getParadas(region);
+    }, 100);
+			
+		});
 
 	});
+}
 }
 
 Titanium.Geolocation.addEventListener('location', function() {
@@ -79,23 +127,25 @@ function getMapBounds(region) {
 	b.southWest = {};
 	b.southEast = {};
 
-	b.northWest.lat = parseFloat(region.latitude) + parseFloat(region.latitudeDelta) / 2.0;
-	b.northWest.lng = parseFloat(region.longitude) - parseFloat(region.longitudeDelta) / 2.0;
+	b.northWest.lat = region.latitude + (region.latitudeDelta / 2.0);
+	b.northWest.lng = region.longitude - (region.longitudeDelta / 2.0);
 
-	b.southWest.lat = parseFloat(region.latitude) - parseFloat(region.latitudeDelta) / 2.0;
-	b.southWest.lng = parseFloat(region.longitude) - parseFloat(region.longitudeDelta) / 2.0;
+	b.southWest.lat = region.latitude - (region.latitudeDelta / 2.0);
+	b.southWest.lng = region.longitude - (region.longitudeDelta / 2.0);
 
-	b.northEast.lat = parseFloat(region.latitude) + parseFloat(region.latitudeDelta) / 2.0;
-	b.northEast.lng = parseFloat(region.longitude) + parseFloat(region.longitudeDelta) / 2.0;
+	b.northEast.lat = region.latitude + (region.latitudeDelta / 2.0);
+	b.northEast.lng = region.longitude + (region.longitudeDelta / 2.0);
 
-	b.southEast.lat = parseFloat(region.latitude) - parseFloat(region.latitudeDelta) / 2.0;
-	b.southEast.lng = parseFloat(region.longitude) + parseFloat(region.longitudeDelta) / 2.0;
+	b.southEast.lat = region.latitude - (region.latitudeDelta / 2.0);
+	b.southEast.lng = region.longitude + (region.longitudeDelta / 2.0);
 
 	return b;
 }
 
 function getParadas(region) {
 
+	Ti.API.log('LatitudDelta: '+ region.latitudeDelta);
+	var delta = region.latitudeDelta;
 	var bounds = getMapBounds(region);
 	var url = "http://transporteactivo.com/api/v1/paradas-cercanas/?ne="+bounds.northEast.lat+","+bounds.northEast.lng+"&sw="+bounds.southWest.lat+","+bounds.southWest.lng;
 	Ti.API.log('URL: '+ url);
@@ -103,13 +153,46 @@ function getParadas(region) {
  
 var xhr = Ti.Network.createHTTPClient({
     onload: function() {
+    paradas = new Array();
+    mapview.removeAllAnnotations();
+    
+    
     Ti.API.log('RESPUESTA: '+ this.responseText);
  	
     json = JSON.parse(this.responseText);
     for (i = 0; i < json.length; i++) {
         parada = json[i];
-       	Ti.API.log("MARKER: " + parada.id);
+        var imagen;
+        
+        if(parada.tipo_parada===1){
+        	imagen = '/images/marker_icon_troncal.png';
+        } else
+         if(parada.tipo_parada===2){
+        	imagen = '/images/marker_icon_pretroncal.png';
+        } else{
+        	imagen = '/images/marker_icon_alimentadora.png';
+        };
+        
+       	var pin = Titanium.Map.createAnnotation({
+		latitude : parada.lat,
+		longitude : parada.lng,
+		title : parada.nombre,
+		image: imagen,
+		animate : true,
+		myid : i // Custom property to uniquely identify this annotation.
+	}); 
+	
+	if(delta <0.025){
+	paradas.push(pin);
+	}else
+	{
+		if(parada.tipo_parada===1){
+			paradas.push(pin);
+		}
+	};
         }
+        Ti.API.log('NUMERO DE PARADAS: '+ paradas.length);
+      mapview.addAnnotations(paradas);
     },
     onerror: function(e) {
     Ti.API.log("STATUS: " + this.status);
