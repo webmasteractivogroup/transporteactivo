f01999cc86adb587939c505c5e902fb9e3b9ec79
Matheo <matheof@hotmail.com>
2013-05-15 21:35:14 -0500

    android icon fix

--- frontend/Transporte Activo/Resources/ui/common/ApplicationTabGroup.js
+++ frontend/Transporte Activo/Resources/ui/common/ApplicationTabGroup.js
@@ -9,27 +9,27 @@ function ApplicationTabGroup(Window) {
 		win4 = new Window('Noticias');
 	
 	var tab1 = Ti.UI.createTab({
-		title: L('Planear Viaje'),
+		title: 'Planear Viaje',
 		icon: '/images/map.png',
 		window: win1
 	});
 	
 	
 	var tab2 = Ti.UI.createTab({
-		title: L('Buscar'),
+		title: 'Buscar',
 		icon: '/images/search.png',
 		window: win2
 	});
 	
 	var tab3 = Ti.UI.createTab({
-		title: L('Favoritos'),
+		title: 'Favoritos',
 		icon: '/images/favoritos.png',
 		window: win3
 	});
 	
 	
 	var tab4 = Ti.UI.createTab({
-		title: L('Noticias'),
+		title: 'Noticias',
 		icon: '/images/news.png',
 		window: win4
 	});
