35a2f0bb5aecd4a2d995b42b5cbd4f378540ee01
Guillermo Álvarez <guialante@gmail.com>
2013-06-12 12:09:54 -0500

    se crea vista una vista generica para servir el index desde django

--- backend/transporteactivo/transporteactivo/urls.py
+++ backend/transporteactivo/transporteactivo/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
-# from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
 from rest_framework.routers import DefaultRouter
 
@@ -19,6 +20,8 @@ router.register(r'buscar-parada', views.BusquedaParadasViewSet)
 
 urlpatterns = patterns('',
 
-    url(r'^api/v1/', include(router.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^v1/', include(router.urls)),
     # url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += staticfiles_urlpatterns()
